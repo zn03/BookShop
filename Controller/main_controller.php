e7e3996ee3316a6cc3c7d46a4d199c44259b2a4c
Duy Nguyen <duynguyen.1112003@gmail.com>
2023-03-22 15:05:43 +0700

    xong product r nhe

--- Controller/main_controller.php
+++ Controller/main_controller.php
@@ -1,12 +1,32 @@
 <?php
-//Lấy giá trị Controller nào đang làm việc với Client
-$controller = $_GET['controller']  ?? '';
-// Điều khiển controller làm gì
+// Lấy giá trị Controller nào đang làm việc với Client
+$controller = $_GET['controller'] ?? '';
+// Điều khiển Controller làm gì
 $action = $_GET['action'] ?? '';
 // Gọi chức năng cho Client
 switch($controller) {
-    case 'user' : require_once('Admin/customer.php'); break;
-    case 'product' : require_once('Product/product_controller.php'); break;
-    
+    case '' :
+        if(isset($_SESSION['user_name']) && isset($_SESSION['password'])) {
+            require_once('Views/index.php');
+        }else{
+            header('location: ?controller=login&action=login');
+        }
+        ; break;
+    case 'login' : require_once('login/login_controller.php'); break;
+    case 'user' : 
+        if(isset($_SESSION['user_name']) && isset($_SESSION['password'])) {
+            require_once('User/user_controller.php');
+        }else{
+            header('location: ?controller=login&action=login');
+        }
+        ; break;
+    case 'product' : 
+        if(isset($_SESSION['user_name']) && isset($_SESSION['password'])) {
+            require_once('Product/product_controller.php');
+        }else{
+            header('location: ?controller=login&action=login');
+        }
+        ; break;
 }
+
 ?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+    session_start();
+    require_once('Controller/main_controller.php');
+    
+?>
